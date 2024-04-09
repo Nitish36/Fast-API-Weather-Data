ea6faa5336abefe80d58d7692c702eb23d958dd1
Nitish <1rn19cs092.nitish.pkv@gmail.com>
2024-04-09 21:42:33 +0530

    Final Commit

--- fastAPIS.py
+++ fastAPIS.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI,Path
 import json
-import uvicorn
 app = FastAPI()
 
-@app.get("/get")
+@app.get("/")
 def home():
     with open('LiveWeatherData.json', 'r') as file:
         jsoneddata = json.load(file)
         return jsoneddata
-if __name__ == "__main__":
-    uvicorn.run("fastAPIS:app", host="127.0.0.1", port=10000, log_level="info")
