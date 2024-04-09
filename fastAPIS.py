8be3154af6c0ef6ef6cd6743aba6db06f1c14bc5
Nitish <1rn19cs092.nitish.pkv@gmail.com>
2024-04-09 21:25:49 +0530

    Final Commit

--- fastAPIS.py
+++ fastAPIS.py
@@ -9,4 +9,4 @@ def home():
         jsoneddata = json.load(file)
         return jsoneddata
 if __name__ == "__main__":
-    uvicorn.run("fastAPIS:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("fastAPIS:app", host="127.0.0.1", port=10000, log_level="info")
